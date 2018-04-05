eed8b3ae12019246dec615afe877909309212d80
Yoeri van de Noort <faint0rz@gmail.com>
2018-04-05 14:33:24 +0200

    initial

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		print(player);
+
+	}
+	private void LateUpdate() {
+	transform.position = player.transform.position;		
+	}
+}
