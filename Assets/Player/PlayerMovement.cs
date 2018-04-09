8439ce14df536e533f659dc567cc35203a5333a0
Unknown <faint0rz@gmail.com>
2018-04-09 23:55:43 +0200

    Added animations
    added jump animation
    added run animation
    removed standard assets

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10.0F;
+    [SerializeField] float controllSpeed = 1f;
+    [SerializeField] float jumpPower = 12f;
+    Rigidbody playerBody;
+    static Animator animator;
+    float translation;
+    [SerializeField] bool isGrounded;
+    [SerializeField] float yVelocity;
+    bool facingRight;
+    int jumpCount;
+
+
+    private void Awake() {
+     	gameObject.tag = "Player";   
+    }
+
+	private void Start() {
+    facingRight = true;
+    animator = GetComponent<Animator>();
+	playerBody = GetComponent<Rigidbody>();
+	
+	}
+	void OnCollisionEnter(Collision playerCollider) {
+		if (!isGrounded)
+		{
+			isGrounded = true;
+            jumpCount = 1;
+            animator.ResetTrigger("isJumping");
+            animator.SetBool("isLanding", false);
+		}
+	}
+	private void Update()
+    {
+        PlayerJump();  
+        RunningAnimation();      
+        yVelocity = playerBody.velocity.y;
+        
+    }
+
+    void FixedUpdate() 
+    {
+        InputTranslator();
+        flipPlayer();
+        RunningAnimation();
+        LayerHandler();
+        
+    }
+
+    private void InputTranslator()
+    {
+        translation = Input.GetAxis("Horizontal") * speed;
+        MovementHandeler();
+    }
+
+    private void flipPlayer()
+    {
+        if (translation > 0 && !facingRight || translation < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.z *= -1;
+            transform.localScale = (localScale);
+        }
+    }
+
+    private void RunningAnimation()
+    {
+        if (translation != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    private void MovementHandeler()
+    {
+        if (playerBody.velocity.y < -1)
+        {   
+            animator.SetBool("isLanding", true);
+        }
+
+        translation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+
+    }
+
+    private void PlayerJump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && isGrounded && jumpCount > 0)
+        {   
+            jumpCount -= 1;
+            animator.SetTrigger("isJumping");
+            playerBody.velocity = new Vector3(playerBody.velocity.x,jumpPower,playerBody.velocity.z);
+			isGrounded = false;
+        }
+    }
+
+    private void LayerHandler() 
+    {
+        if (!isGrounded)
+        {
+            animator.SetLayerWeight(1,1);
+        }
+        else
+        {
+            animator.SetLayerWeight(1,0);
+        }
+    }
+}
