ba694252f7a665fcf57b22de15d9c372c3617594
Unknown <faint0rz@gmail.com>
2018-04-26 15:46:32 +0200

    Added Skybox and Jump text

---
+++ Assets/JumpTextTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTextTrigger : MonoBehaviour {
+
+	GameObject textBalloon;
+
+	private void Start() {
+		textBalloon = transform.Find("TextBalloon").gameObject;
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player")
+		{
+			textBalloon.SetActive(true);
+		}
+	}
+}
